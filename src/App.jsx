ee5591dbe86249c99a7d3a2b987b09573ed841d4
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-06-12 16:14:45 +0800

    Add routing

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MainLayout from "./layout/MainLayout";
+import Register from "./pages/SignUpPage/SignUpPage";
+import Login from "./pages/LogInPage/LogInPage";
+import Dashboard from "./pages/Dashboard/DashBoardPage";
+
+
+
+  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Register />,
+      },
+      {
+        path: "dashboard",
+        element: 
+          <Dashboard />
+        ,
+      },
+    ],
+  },
+]);
+    
+  
+
+
+export default function App(){
+   return <RouterProvider router={router} />
+};
