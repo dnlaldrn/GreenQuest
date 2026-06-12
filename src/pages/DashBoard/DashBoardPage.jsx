2f5e2e6f1dd8e1c81e3b6780d552b01c837984bc
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-06-12 16:17:44 +0800

    Add dashboard page

---
+++ src/pages/DashBoard/DashBoardPage.jsx
@@ -0,0 +1,9 @@
+import Dashboard from "../../components/DashBoadComponent";
+
+export default function DashBoardPage(){
+    return (
+        <div>
+            <Dashboard/>
+        </div>    
+        )
+}
