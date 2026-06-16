93c1e11963c9860aba2f90aebe7f44e5bd88e59d
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-06-17 05:50:38 +0800

    modify the login page

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <p>Login cijfneijnfejnf</p>
+    <form >
+      <label>Enter your email address:
+        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)} } />
+      </label>
+      <label>Enter your password:
+        <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+      </label>
+      <button>Log In</button>
+    </form>
+    
   )
  
   ;
