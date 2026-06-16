c34f87758ab72036955453a4dcfdea42ff0b34f1
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-06-16 23:57:28 +0800

    Add login text and sign up text'

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,40 +1,9 @@
-import { useState } from "react";
-import { supabase } from "../lib/supabase";
+
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function handleLogin(e) {
-    e.preventDefault();
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      alert(error.message);
-    } else {
-      alert("Logged in!");
-    }
-  }
-
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button type="submit">Login</button>
-    </form>
-  );
+    <p>Login cijfneijnfejnf</p>
+  )
+ 
+  ;
 }
--- src/components/SignUpComponent.jsx
+++ src/components/SignUpComponent.jsx
@@ -1,40 +1,9 @@
-import { useState } from "react";
-import { supabase } from "../lib/supabase";
 
-export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  async function handleRegister(e) {
-    e.preventDefault();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-    });
-
-    if (error) {
-      alert(error.message);
-    } else {
-      alert("Registration successful!");
-    }
-  }
-
+export default function SignUpComponent() {
   return (
-    <form onSubmit={handleRegister}>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button type="submit">
-        Register
-      </button>
-    </form>
-  );
+    <p>Sign Up form</p>
+  )
+ 
+  ;
 }
