6ca7cd360676472068c0001e6bd327422c35550c
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-06-18 16:02:42 +0800

    MOdify the sign up component

--- src/components/SignUpComponent.jsx
+++ src/components/SignUpComponent.jsx
@@ -1,16 +1,19 @@
-
+import { useState } from 'react';
 
 export default function SignUpComponent() {
+  const [username, setUserName] = useState("")
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
   return (
      <form className="h-screen flex">
       <label>Enter your username:
-        <input type="text" />
+        <input type="text" value={username} onChange={(e)=>{setUserName(e.target.value)}}/>
       </label>
       <label>Enter your email address:
-        <input type="text" />
+        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
       </label>
       <label>Enter your password:
-        <input type="password" />
+        <input type="password"  value={password} onChange={(e)=>{setPassword(e.target.value)}} />
       </label>
       <label>Confirm your password:
         <input type="password" />
