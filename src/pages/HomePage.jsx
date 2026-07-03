c8678f9e4b369c96f5c45334763adb169e868167
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2026-07-04 00:49:22 +0800

    Add link to the homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function HeroSection() {
   const steps = [
@@ -283,7 +284,7 @@ export default function HeroSection() {
                   d="M12 4v16m8-8H4"
                 />
               </svg>
-              <span>Upload Video</span>
+              <Link to="/userDasboard">Upload Video</Link>
             </button>
 
             <button className="border border-emerald-500/30 hover:border-emerald-400 bg-emerald-950/20 hover:bg-emerald-950/40 text-emerald-400 px-6 py-3 rounded-lg font-semibold tracking-wide transition-all duration-200">
